18c8b77b6624e22eaa688356d25084690c879e26
carlosmsevi <carlosmsevillab28@gmail.com>
2026-01-28 11:50:17 -0400

    Hemos desarrollado una configuracion de logging

---
+++ app/settings/setup_logging.py
@@ -0,0 +1,18 @@
+import logging
+from typing import Optional
+from app.settings.app_settings import Settings
+
+def setup_logging(model_name: Optional[str] = None) -> None:
+    """
+    Función para establecer la configuración de registro básica.
+    """
+    log_file_name: str = f"{Settings.MODEL_NAME if not model_name else model_name}.log"
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] [%(name)s]: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[
+            logging.FileHandler(str(Settings.LOG_DIR / log_file_name), encoding="utf-8"),
+            logging.StreamHandler()
+        ]
+    )
