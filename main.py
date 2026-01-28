96da247b82225f0e4c82392759c61fb2e18f53b9
carlosmsevi <carlosmsevillab28@gmail.com>
2026-01-28 11:57:36 -0400

    Prueba de script de entrenamiento del modulo Franchesco

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import torch
 from app.bpetokenizer.bpetokenizer import BPETokenizer
 from app.embeddings.embeddings import TransformerEmbedding
+from app.settings.app_settings import Settings
 
-VOCAB_SIZE = 5000
-D_MODEL = 256
-MAX_SEQ_LEN = 256
-
-tokenizer = BPETokenizer(vocab_size=VOCAB_SIZE)
+tokenizer = BPETokenizer(vocab_size=Settings.VOCAB_SIZE)
 
 tokenizer.train(["test.txt"])
-tokenizer.save("assets/model_tokens.json")
+tokenizer.save(str(Settings.ASSETS_DIR / "model_tokens.json"))
 
 texto_prueba = "La navidad es satanica porque yisus no nacio el 25 de diciembre"
 input_ids = tokenizer.encode(texto_prueba)
 print(input_ids)
 
-embedder = TransformerEmbedding(vocab_size=VOCAB_SIZE, d_model= D_MODEL, max_seq_len=MAX_SEQ_LEN)
+embedder = TransformerEmbedding(vocab_size=Settings.VOCAB_SIZE, d_model= Settings.D_MODEL, max_seq_len=Settings.MAX_SEQ_LEN)
 
 input_tensor = torch.tensor(input_ids).unsqueeze(0)
 
---
+++ train_franchesco.py
@@ -0,0 +1,40 @@
+import logging
+from pathlib import Path
+from app.trainer import TrainModule
+from app.settings.app_settings import Settings
+from app.settings.setup_logging import setup_logging
+
+def main():
+    # 1. Configuración de logs para ver qué ocurre internamente
+    setup_logging()
+    logger = logging.getLogger("TestValidation")
+
+    # 2. Definir ruta del dataset pequeño
+    dataset_small: Path = Settings.ASSETS_DIR / "recetas.txt"
+    
+    if not dataset_small.exists():
+        logger.error(f"No se encontró el archivo en {dataset_small}")
+        return
+
+    # 3. Instanciar el Trainer
+    # Usamos la configuración de Config directamente
+    trainer = TrainModule(
+        config=Settings, 
+        dataset_path=dataset_small, 
+        model_name=Settings.MODEL_NAME
+        )
+
+    # 4. (Opcional) Intentar cargar un checkpoint si quieres probar esa lógica
+    # trainer.load_checkpoint(Config.MODEL_DIR / "checkpoint_epoch_2.pth")
+
+    try:
+        logger.info("Iniciando prueba de validación corta...")
+        # Ejecutamos el entrenamiento
+        trainer.train()
+        logger.info("¡Validación exitosa! El ciclo de entrenamiento completó correctamente.")
+        
+    except Exception as e:
+        logger.error(f"Error durante la validación: {e}", exc_info=True)
+
+if __name__ == "__main__":
+    main()
